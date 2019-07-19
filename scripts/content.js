a187eeb939d78ff8bd0e7ab6838327cfcf354b41
Tandoori Momos <yash20008@gmail.com>
2019-07-19 19:52:49 +0530

    Added pinned notes

--- scripts/content.js
+++ scripts/content.js
@@ -12,6 +12,25 @@ function Tab(url, id) {
     }
 }
 
+function Article (url, text) {
+    
+    this.text = text;
+    this.url = url;
+
+    this.setText = function(newText) {
+        this.text = newText;
+        console.log('Text updated!');
+    }
+
+    this.getText = function() {
+        return this.text;
+    }
+
+    this.getUrl = function() {
+        return this.url;
+    }
+}
+
 // Create a new Tab instance
 let tab = new Tab(null, 0);
 
@@ -20,6 +39,7 @@ function getSelectedText() {
     let text; 
     if(window.getSelection) {
         text = window.getSelection().toString();
+        console.log(window.getSelection());
     }
 
     return text;
@@ -29,6 +49,7 @@ function getSelectedText() {
 // Send request to get the current url
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
+        console.log(message);
         tab.updateParams({
             url: message.url,
             id: message.id
@@ -36,10 +57,40 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-window.onload = function() {   
-    console.log('Content Script Mounted!'); 
-    window.addEventListener("mouseup", function() {
-        console.log(getSelectedText());
-        console.log(tab.getUrl());
+
+console.log('Content Script Mounted!'); 
+
+window.addEventListener("mouseup", function() {
+    let article = new Article(tab.getUrl(), getSelectedText());
+
+    console.log(article.getText());
+         
+    // Create pinned box for data
+    function createPin(article) {
+        try {
+            // Clean out all pre-existing notes
+            [...document.getElementsByClassName('pinnedNote')].map(el => el.remove());
+
+            // Create a new note
+            let pinnedNote = document.createElement('div');
+            pinnedNote.setAttribute('class', 'pinnedNote');
+
+            // Set the data for the note
+            pinnedNote.innerHTML = '<h1>' + article.getText() + '</h1>';
+
+            // Mount the style for the note 
+                
+
+            // Mount the generate note to the DOM
+            (document.getElementsByTagName('body')[0]).appendChild(pinnedNote);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            console.log('Created pinned note!');
+        }
+    }
+        
+    createPin(article);
+    
+       
     });  
-}
