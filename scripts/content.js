ddb2098f1c7e3c538319122bebed9b78b60bc5ee
Tandoori Momos <yash20008@gmail.com>
2019-07-20 18:35:38 +0530

    Improved popup logic

--- scripts/content.js
+++ scripts/content.js
@@ -13,21 +13,6 @@ function Tab(url, id) {
     }
 }
 
-function Article (text) {
-    
-    this.text = text;
-
-    this.setText = function(newText) {
-        this.text = newText;
-        console.log('Text updated!');
-    }
-
-}
-
-// Create a new Tab instance
-let tab = new Tab(null, 0);
-
-
 function getSelectedText() {
     let text; 
     if(window.getSelection) {
@@ -38,66 +23,79 @@ function getSelectedText() {
     return text;
 }
 
-// Create pinned box for data
-    function createPin(article) {
-    	console.time('pinBlock');
-        try {
-            // Clean out all pre-existing iframe
-            [...document.getElementsByClassName('generatedIframe')].map(el => el.remove());
-
-            //  The pinned note must be created within an iframe context 
-            //  to prevent it from inheriting styles 
-
-            let iframe = document.createElement('iframe');
-            iframe.setAttribute('class', 'generatedIframe');
-            (document.getElementsByTagName('body')[0]).appendChild(iframe);
-          
-
-            let iframeContext = iframe.contentDocument || iframe.contentWindow.document;
-
-            // Write onto the iframe
-            iframeContext.open();
-            iframeContext.write('<html><body></body></html>');
-            iframeContext.close();
-
-            iframeBody = iframeContext.body;
-               
-            let pinnedNote = document.createElement('div');
-            pinnedNote.setAttribute('class', 'pinnedNote');
-
-            // Set the data for the note
-            pinnedNote.innerHTML = '<h1>' + article.text + '</h1>';
-
-            pinnedNote.style.fontSize = '8px';
-            pinnedNote.style.color = '#fff';
-            pinnedNote.style.fontFamily = 'Segoe ui, sans-serif, Impact';
-
-            // Mount the generate note to the iframe
-            iframeBody.appendChild(pinnedNote);
-            console.log('Created pinned note!');
-        } catch (err) {
-            console.error(err);
-        } finally {
-           	console.timeEnd('pinBlock');
-        }
+// Send request to get the current url
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        createPin(article);
+        console.log(message);
+        tab.updateParams({
+            url: message.url,
+            id: message.id
+        });
     }
+);
 
-window.addEventListener("mouseup", function() {
-  	
-  	let article = new Article(getSelectedText());
-  	
-  	try {
-		// Send request to get the current url
-		chrome.runtime.onMessage.addListener(
-		    function(message, sender, sendResponse) {
-		        createPin(article);
-		        tab.updateParams({
-		            url: message.url,
-		            id: message.id
-		        });
-		    }
-		);
-	} catch (err) {
+// Create an empty article and an instance of a Tab
+let tab = new Tab(null, 0);
+
+let article = {
+    text: undefined
+}
+
+// Create pinned box for data
+function createPin(article) {
+   	console.time('pinBlock');
+    try {
+        // Clean out all pre-existing iframe
+        [...document.getElementsByClassName('generatedIframe')].map(el => el.remove());
+
+        //  The pinned note must be created within an iframe context 
+        //  to prevent it from inheriting styles 
+
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('class', 'generatedIframe');
+        (document.getElementsByTagName('body')[0]).appendChild(iframe);
+         
+        let iframeContext = iframe.contentDocument || iframe.contentWindow.document;
+
+        // Write onto the iframe
+        iframeContext.open();
+        iframeContext.write('<html><body></body></html>');
+        iframeContext.close();
+
+        iframeBody = iframeContext.body;
+            
+        let pinnedNote = document.createElement('div');
+        pinnedNote.setAttribute('class', 'pinnedNote');
+
+        // Set the data for the note
+        pinnedNote.innerHTML = '<h1>' + article.text + '</h1>';
+
+        pinnedNote.style.fontSize = '8px';
+        pinnedNote.style.color = '#fff';
+        pinnedNote.style.fontFamily = 'Segoe ui, sans-serif, Impact';
+
+        // Mount the generate note to the iframe
+        iframeBody.appendChild(pinnedNote);
+        console.log('Created pinned note!');
+    } catch (err) {
+        console.error(err);
+    } finally {
+      	console.timeEnd('pinBlock');
+    }
+}
+
+window.addEventListener("mouseup", function() {  
+    
+    try {
+        if (getSelectedText()) {
+            article.text = getSelectedText();
+            
+            if (tab.url) {
+                createPin(article);
+            }
+        }
+    } catch (err) {
 		console.error('Error');
 	} 
 });  
