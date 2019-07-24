7a7a31c830300c91e259ccefd577c048a39efeeb
Tandoori Momos <yash20008@gmail.com>
2019-07-24 17:28:55 +0530

    Minor edits

--- src/routes/api/api.routes.js
+++ src/routes/api/api.routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/article', urlencodedParser ,(req,res) => {
-    winston.info(req.body);
+    console.log(req.body);
     return res.status(200).json({ title: title });
 });
 
