4d6eda503751092a6dd82cb5eafed26fa3f47e90
Tandoori Momos <yash20008@gmail.com>
2019-08-04 17:28:51 +0530

    Minor edits|

--- src/routes/api/api.routes.js
+++ src/routes/api/api.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import FetchArticle from './../../utils/FetchArticle';
-import winston from './../../config/winston';
+
+import logger from './../../config/winston';
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -14,14 +15,25 @@ router.get('/', (req, res) => {
 router.get('/article', (req,res) => {
     FetchArticle({
         url: "thehindu"
-    });
+    })
+    .then((doc) => {
+        res.json(doc)
+    })
+    .catch((err) => logger.error(err));
 });
 
 router.post('/article', urlencodedParser ,(req,res) => {
-    return res.status(200).json({ 
-        title: req.body.text,
+    FetchArticle({
         url: req.body.url
-    });
+    })
+    .then((doc) => {
+        res.status(200).json({ 
+            title: req.body.text,
+            url: req.body.url
+        })
+    })
+    .catch((err) => logger.error(err));
+
 });
 
 export default router;
