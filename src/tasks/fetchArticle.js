b2c2279ff16d068b44792e6d7410b76919a3e3e2
Tandoori Momos <yash20008@gmail.com>
2019-08-24 00:21:21 +0530

    Added async

--- src/tasks/fetchArticle.js
+++ src/tasks/fetchArticle.js
@@ -6,10 +6,26 @@ import MediaListing from '../models/media-model';
 import logger from '../config/winston';
 import request from 'request';
 import tokenize from './../utils/tokenize';
+import async from 'async';
 import { JaroWinklerDistance } from 'natural';
 import { parseString } from 'xml2js';
 import shuffleArray from './../utils/shuffle';
 import compareTokens from './../utils/compareTokens';
+
+const requestArticle = async function(keywords, source) {
+    return new Promise((resolve, reject) {
+        request(`https://newsapi.org/v2/everything?q=trump+el+paso&domains=foxnews.com&apiKey=17279e5e52c04dc1a189434c07aab8df`, function(err, response, body) {
+            if (err) { reject(err); }
+
+            if (response.statusCode === 200) {
+                body = JSON.parse(body);
+                let articles = body.articles;
+                
+                
+            }
+        });
+    });
+}
 
 const fetchArticle =  async (sourceArticle, sourceBias) => {
     return new Promise((resolve, reject) => {
@@ -43,48 +59,27 @@ const fetchArticle =  async (sourceArticle, sourceBias) => {
         MediaListing.find({
             bias: newArticle.bias
         }, function(err, docs) {
+
             let dbSources = [];
             for (let doc of docs) {
                 dbSources.push(doc.name);
             }
             
+            // Shuffle the dbSources array for extra randomness 
+            dbSources = shuffleArray(dbSources);   
+
             // Get tokens from the title in url format
             let keywords_url = tokenize(sourceArticle.title, { returnType: 'url' });
-                  
-            // Shuffle the dbSources array for extra randomness 
-            dbSources = shuffleArray(dbSources);
+            
+            async.each(dbSources, function(source, callback) {
+                console.log('Processing for ', source);
 
-            let source = dbSources[Math.floor(Math.random() * dbSources.length)];             
-            request(`https://news.google.com/rss/search?q=${keywords_url}+${ encodeURIComponent(source)}`, function(err, response, body) {
-                if(response.statusCode === 200 && !err) {  
-                    // Convert RSS feed to JSON format
-                    parseString(body, function(err,result) {
-                        if (err) reject(err);
-                         if (result.rss.channel[0].item) {
-                            let items = shuffleArray(result.rss.channel[0].item);
-                            for (let j = 0; j < items.length; j++) {
-                                // Select an article whose source name matches the most to the fetched sources from the DB 
-                                if (typeof items[j].source !== undefined) {
-                                    if (JaroWinklerDistance(items[j].source[0]._, source, undefined, true) >= 0.5) {
-                                        newArticle.title = items[j].title[0];
-                                       
-                                        // Returns a boolean which indicates if the keywords of the 
-                                        // source article and new article are similar or not
-                                        if(compareTokens(sourceArticle.title, newArticle.title)) {
-                                            newArticle.source = source;
-                                            newArticle.link = items[j].link[0];
-                                            resolve(newArticle);     
-                                        }
-                                        break; // Break the inner loop                            
-                                    }
-                                }
-                            }
-                        } 
-                    });
-                } else {
-                    logger.error(err);
-                }
+                reque
             });
+
+            
+
+           
         });
  
     })
